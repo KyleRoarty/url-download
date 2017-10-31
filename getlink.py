31742f59f898a076b40d045af53a1680791244dd
Kyle Roarty <kroarty@xes-inc.com>
2017-10-31 10:01:57 -0500

    gitlink: Add looping support, use with allitebooks.com
    Signed-off-by: Kyle Roarty <kroarty@xes-inc.com>

--- getlink.py
+++ getlink.py
@@ -15,18 +15,41 @@ def parseArgs():
 
     return parser.parse_args()
 
+def loop(domain, url):
+    response = requests.get(url)
+    slop = BeautifulSoup(response.text, 'html.parser')
+
+    ret_dls = list()
+
+    while True:
+        links = slop.findAll('a', rel='bookmark', href=True, text=True)
+        for link in links:
+            ret_dls += getFiles(domain, link.get('href'))
+
+        if slop.find('link', rel='next', href=True) is None:
+            print "None!"
+            break
+        else:
+            print "Looping"
+            url = slop.find('link', rel='next', href=True).get('href')
+            response = requests.get(url)
+            slop = BeautifulSoup(response.text, 'html.parser')
+
+    return ret_dls
+
 def getFiles(domain, url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    selection = soup.select('u a')
+    selection = soup.findAll('a', href=True, target="_blank")
 
     dls = []
 
     for link in selection:
-        l = link.get('href')
-        if not validators.url(l):
-            dls.append(domain + l)
-        else:
+        if link.find('i', class_="fa fa-download"):
+            l = link.get('href')
+        #if not validators.url(l):
+        #   dls.append(domain + l)
+        #else:
             dls.append(l)
 
     return dls
@@ -37,9 +60,16 @@ def main():
 
     domain = re.match('^http(s*)://.*?(?=/)', pargs.url).group(0)
 
-    dls = getFiles(domain, pargs.url)
+
+    if True:
+        dls = loop(domain, pargs.url)
+    else:
+        dls = getFiles(domain, pargs.url)
+
+
     for dl in dls:
-        wget.download(dl)
+        print dl
+        #wget.download(dl)
 
 if __name__ == '__main__':
     main()
