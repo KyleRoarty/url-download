3d66f42b04932a2b4d6116124d4889a851176ddb
Kyle Roarty <kyleroarty1716@gmail.com>
2017-11-02 06:49:34 -0500

    Better multithreading
    Signed-off-by: Kyle Roarty <kyleroarty1716@gmail.com>

--- getlink.py
+++ getlink.py
@@ -17,29 +17,46 @@ def parseArgs():
 
     return parser.parse_args()
 
+def parseFunc(domain, url):
+    print url
+    response = requests.get(url)
+    sloop = BeautifulSoup(response.text, 'html.parser')
+    links = sloop.findAll('a', rel='bookmark', href=True, text=True)
+
+    futs = list()
+
+    exc = concurrent.futures.ProcessPoolExecutor(10)
+    futs += [exc.submit(getFiles, domain, link.get('href')) for link in links]
+    concurrent.futures.wait(futs)
+
+
+    exc.shutdown(True)
+    return [fut.result() for fut in futs]
+
 def loop(domain, url):
     response = requests.get(url)
     slop = BeautifulSoup(response.text, 'html.parser')
-
+    i = 1
     ret_dls = list()
 
+    urlList = [url]
+
     while True:
-        links = slop.findAll('a', rel='bookmark', href=True, text=True)
-
-        executor = concurrent.futures.ThreadPoolExecutor(10)
-        ret_dls += [executor.submit(getFiles, domain, link.get('href')) for link in links]
-        concurrent.futures.wait(ret_dls)
-
         if slop.find('link', rel='next', href=True) is None:
-            print "None!"
             break
         else:
-            print "Looping"
+            print i
+            i += 1
             url = slop.find('link', rel='next', href=True).get('href')
             response = requests.get(url)
             slop = BeautifulSoup(response.text, 'html.parser')
-
-    return ret_dls
+            urlList += [url]
+    futures = list()
+    exc1 = concurrent.futures.ThreadPoolExecutor(10)
+    futures += [exc1.submit(parseFunc, domain, url) for url in urlList]
+    concurrent.futures.wait(futures)
+    exc1.shutdown(True)
+    return futures
 
 def getFiles(domain, url):
     response = requests.get(url)
@@ -58,8 +75,8 @@ def getFiles(domain, url):
 
     return dls
 
-def download(future):
-    wget.download(''.join(future.result()))
+def download(url):
+    wget.download(''.join(url))
 
 def main():
 
@@ -77,17 +94,17 @@ def main():
         dls = getFiles(domain, pargs.url)
 
 
-
+    print len(dls)
     start = time.time()
-    executor = concurrent.futures.ThreadPoolExecutor(len(dls))
+    dls = [dl.result() for dl in dls]
+    dls = [url for dl in dls for url in dl]
+    print len(dls)
+    executor = concurrent.futures.ThreadPoolExecutor(len(dls)/32)
     futures = [executor.submit(download, dl) for dl in dls]
     concurrent.futures.wait(futures)
     end = time.time()
-    print diff_get_links
+    print "\n"+str(diff_get_links)
     print end-start
-#    for dl in dls:
-#        print ''.join(dl.result())
-#        wget.download(dl)
 
 if __name__ == '__main__':
     main()
